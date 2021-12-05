5485a868c3820c11694296ee06da510e9eec767a
tuukka <tuukka.t.korhonen@protonmail.com>
2021-12-05 21:49:28 +0200

    switched home server to NixOS

--- hosts/vps.nix
+++ hosts/vps.nix
@@ -1,56 +1,57 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-    networking.hostName = "server";
+	networking.firewall.enable = false;
+	nixpkgs.config.allowUnfree = true;
+	services.openssh.enable = true;
+    networking.hostName = "gensokyo";
     imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
     
   boot = {
     initrd = {
-      availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "sd_mod" "sr_mod" ];
+	availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" "sr_mod" ];
     };
-    #kernelModules = [ "" ];
   };
+   fileSystems."/" =
+    { device = "/dev/disk/by-uuid/57c57de3-f4b4-4b22-b606-f64347a1ef7e";
+      fsType = "xfs";
+    };
 
-  fileSystems."/" = {
-    device = "";
-    fsType = "xfs";
-  };
-  
-  filesystems."/mnt/Sakuya" = {
-    device = "";
-    fsType = "";
-  };
-
-  fileSystems."/export/Sakuya" = {
-    device = "/mnt/Sakuya";
-    options = [ "bind" ];
-  };
+  fileSystems."/var" =
+    { device = "/dev/disk/by-uuid/33c5bfe1-0dce-473e-8476-1ac5ef290510";
+      fsType = "xfs";
+    };
 
   fileSystems."/boot" =
-    { device = "";
+    { device = "/dev/disk/by-uuid/A79B-417B";
       fsType = "vfat";
     };
 
   swapDevices =
-    [ { device = ""; }];
-   
-  services.nfs.server = {
-    enable = true;
-    exports = ''
-      /export/Sakuya  192.168.1.1/24(rw,fsid=0,no_subtree_check)
-    '';
-  };
+    [ { device = "/dev/disk/by-uuid/89d9e05c-d4d5-4591-80a5-c19b38ddb6b0"; }
+    ];
+
+  
+  #services.nfs.server = {
+  #  enable = true;
+  #  exports = ''
+  #    /export/Sakuya  192.168.1.1/24(rw,fsid=0,no_subtree_check)
+  #  '';
+  #};
   services.minecraft-server = {
     enable = true;
     eula = true; #required
+    declarative = true;
+    dataDir = "/var/lib/mincecraft";
     serverProperties = {
-      server-ip = "192.168.1.186";
+	server-ip = "192.168.1.186";
       motd = "panin sun mutsiis";
       allow-nether = true;
-      enable-rcon = false;
+      enable-rcon = true;
+      rconPort = 2556;
       op-permission-level = 2;
       view-distance = 17;
-      online-mode = false;
+      online-mode = true;
       max-players = 20;
       leven-name = "porvaristo";
       simulation-distance = 10;

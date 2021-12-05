d9060eadd9ee786a11229186aa5bc45eae42cad9
tuukka <tuukka.t.korhonen@protonmail.com>
2021-12-06 00:20:19 +0200

    aaaaaaaaaaaaaaaaaa

--- hosts/vps.nix
+++ hosts/vps.nix
@@ -56,7 +56,6 @@
     nfs.server = {
       enable = true;
       exports = ''
-        /export/  192.168.1.1/24(rw,fsid=0,no_subtree_check)
         /export/sakuya/  192.168.1.1/24(rw,fsid=0,no_subtree_check)
         /export/koishi/  192.168.1.1/24(rw,fsid=0,no_subtree_check)
       '';

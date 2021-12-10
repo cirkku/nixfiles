7326855414e4ac86f8b35a2b11f09466013e9220
cirkku <tuukka.t.korhonen@protonmail.com>
2021-12-10 20:20:16 +0200

    forgot to add rpc-whitelist

--- hosts/vps.nix
+++ hosts/vps.nix
@@ -92,6 +92,7 @@
       settings = {
         peer-port = 1337;
         rpc-bind-address = "0.0.0.0";
+        rpc-whitelist = "192.168.1.*";
         download-dir = "/mnt/sakuya/torrents/";
         incomplete-dir = "/mnt/sakuya/torrents/.incomplete/";
         incomplete-dir-enabled = true;
